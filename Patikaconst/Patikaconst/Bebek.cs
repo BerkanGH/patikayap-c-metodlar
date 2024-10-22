298fb51fbbea8f5c638ea61fee6d90a62dc90a85
BerkanGH <berkankeskin28@gmail.com>
2024-10-22 16:52:53 +0300

    odev

---
+++ Patikaconst/Patikaconst/Bebek.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Patikaconst
+{
+    public class Bebek
+    {
+        private string ad;
+        private string soyad;
+        private DateTime dogumtarihi;
+
+        public string Ad { get { return ad; } set { ad = value; } }
+
+        public string Soyad { get { return soyad; } set { soyad = value; } }
+
+        public DateTime Dogumtarihi { get { return dogumtarihi; } set { dogumtarihi = value; } }
+
+        //default olan bu
+        public Bebek()
+        {
+            dogumtarihi = DateTime.Now;
+            Console.WriteLine("ıngaaaa");
+
+        }
+        // 
+        public Bebek(string ad, string soyad)
+        {
+
+            Ad = ad;
+            Soyad = soyad;
+            Console.WriteLine("ıngaaaa");
+
+
+
+
+        }
+
+
+        public void bilgileriGöster()
+        {
+
+            Console.WriteLine($"bebeğin adı {ad} bebeğin soyadı {soyad} bebeğin doğum tarihi{dogumtarihi}");
+
+
+        }
+
+    }
+}
---
+++ Patikaconst/Patikaconst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Patikaconst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default Constructor ile bebek nesnesi oluşturdum
+            Bebek bebek1 = new Bebek();
+            bebek1.Ad = "Mehmet";
+            bebek1.Soyad = "Yılmaz";
+
+            // Parametreli Constructor ile bebek nesnesi oluşturdum
+            Bebek bebek2 = new Bebek("Pelin", "Selçuk");
+
+            // Bilgilerin konsola yazdırılması
+            Console.WriteLine("Bebek 1 Bilgileri:");
+            bebek1.bilgileriGöster();
+
+            Console.WriteLine("\nBebek 2 Bilgileri:");
+            bebek2.bilgileriGöster();
+
+            Console.ReadLine();
+        }
+    }
+}
